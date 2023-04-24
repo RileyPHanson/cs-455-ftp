cd611eee3a14c2c1b00d18785ae452c7c0ef5bb7
RileyPHanson <119458459+RileyPHanson@users.noreply.github.com>
2023-04-23 23:19:20 -0400

    Update client.c

--- client.c
+++ client.c
@@ -17,17 +17,17 @@ void receive_data(int sock_fd, char *buffer, size_t buffer_size) {
 }
 
 int main() {
-    // Get user input for server address, port, username, password, and file to download
+    // Get user input for server address, port
     char server_address[MAX_SIZE];
     int server_port;
-
     char file[MAX_SIZE];
+    char dir[MAX_SIZE];
+    
     printf("Enter server address: ");
     scanf("%s", server_address);
     printf("Enter server port: ");
     scanf("%d", &server_port);
-    printf("Enter file to download: ");
-    scanf("%s", file);
+
 
     // Create a socket
     int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -55,31 +55,75 @@ int main() {
     receive_data(sock_fd, buffer, MAX_SIZE);
     printf("%s", buffer);
 
-    // Send a command to change the current directory
-    sprintf(buffer, "CWD %s\r\n", file);
-    send(sock_fd, buffer, strlen(buffer), 0);
-    receive_data(sock_fd, buffer, MAX_SIZE);
-    printf("%s", buffer);
+    // Get the users commands
+    int running = 1;
+    char input[MAX_SIZE];
+    char destination[MAX_SIZE];
+    char fileOrDir[MAX_SIZE];
+    int i;
+    
 
-    // Send a command to download the file
-    sprintf(buffer, "RETR %s\r\n", file);
-    send(sock_fd, buffer, strlen(buffer), 0);
-    receive_data(sock_fd, buffer, MAX_SIZE);
-    printf("%s", buffer);
+    while(running) {
+        printf("Please enter a command: CWD 'dir', STOP, RETR 'file'\n");
+        fgets(input, sizeof(input), stdin);
+        
+        // Find the command keyword
+        for (i = 0; i < sizeof(input); i++) {
+            if (input[i] == ' ' || input[i] == '\0' || input[i] == '\n') {
+                break;
+            }
+            destination[i] = input[i];
+        }   
+        destination[i] = '\0';
 
-    // Open a file to write the data to
-    FILE *fp = fopen(file, "wb");
-    if (fp == NULL) {
-        perror("file opening failed");
-        exit(EXIT_FAILURE);
+        // Stop if STOP command is used
+        if(strcmp(destination, "STOP") == 0) {
+            running = 0;
+        }
+
+        // Find the file/Dir
+        for (i = 0; i < sizeof(input); i++) {
+            if (input[i] == ' ') {
+                int j = 0;
+                for(i = i + 1; i < sizeof(input); i++) {
+                    if(input[i] == '\n' || input[i] == '\0'){
+                        break;
+                    }
+                    fileOrDir[j] = input[i];
+                    j++;
+                }   
+                break;            
+            }
+        }   
+        // Change Dir
+        if (strcmp(destination, "CWD") == 0) {
+            sprintf(buffer, "CWD %s\r\n", fileOrDir);
+            send(sock_fd, buffer, strlen(buffer), 0);
+            receive_data(sock_fd, buffer, MAX_SIZE);
+            printf("%s", buffer);
+        } 
+        // Get file
+        else if (strcmp(destination, "RETR") == 0) {
+            sprintf(buffer, "RETR %s\r\n", fileOrDir);
+            send(sock_fd, buffer, strlen(buffer), 0);
+            receive_data(sock_fd, buffer, MAX_SIZE);
+            printf("%s", buffer);
+
+            // Open a file to write the data to
+            FILE *fp = fopen(fileOrDir, "wb");
+            if (fp == NULL) {
+                perror("file opening failed");
+                exit(EXIT_FAILURE);
+            }
+
+            // Read the data from the socket and write it to the file
+            int bytes_received = 0;
+            while ((bytes_received = recv(sock_fd, buffer, MAX_SIZE, 0)) > 0) {
+                fwrite(buffer, 1, bytes_received, fp);
+            }
+            fclose(fp);
+        }
     }
-
-    // Read the data from the socket and write it to the file
-    int bytes_received = 0;
-    while ((bytes_received = recv(sock_fd, buffer, MAX_SIZE, 0)) > 0) {
-        fwrite(buffer, 1, bytes_received, fp);
-    }
-    fclose(fp);
 
     // Close the socket
     close(sock_fd);
